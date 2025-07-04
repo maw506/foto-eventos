e9e03b17e5298b45b22ca3af0cd2a6f1d1c0c65b
maw506 <gigenamauro@gmail.com>
2025-07-03 22:55:46 -0300

    Añadir nuevas imágenes a la galería y actualizar el carrusel de fotos; eliminar estilos de galería obsoletos.

--- src/components/reactComponents/HeroCarrousel.jsx
+++ src/components/reactComponents/HeroCarrousel.jsx
@@ -1,6 +1,14 @@
 import { useEffect } from "react";
 
-const fotos = ["pic1.jpg", "pic2.jpg", "pic3.jpg", "pic4.jpg","pic5.jpg", "pic6.jpg", "pic7.jpg","pic8.jpg","pic9.jpg","pic10.jpg","pic11.jpg","pic12.jpg","pic13.jpg"];
+const fotos = [
+  "pic-14.jpg", "pic-15.jpg", "pic-16.jpg", "pic-18.jpg",
+  "pic-19.jpg", "pic-20.jpg", "pic-21.jpg", "pic-22.jpg", "pic-23.jpg",
+  "pic-24.jpg", "pic-25.jpg", "pic-26.jpg", "pic-27.jpg",
+  "pic-29.jpg", "pic-30.jpg", "pic-31.jpg", "pic-32.jpg", "pic-33.jpg",
+  "pic-34.jpg", "pic-35.jpg", "pic-36.jpg", "pic-37.jpg", "pic-38.jpg",
+  "pic-39.jpg", "pic-40.jpg", "pic-41.jpg", "pic-42.jpg", "pic-43.jpg",
+  "pic-44.jpg", "pic-45.jpg", "pic-46.jpg", "pic-47.jpg"
+];
 
 export default function HeroCarousel() {
   useEffect(() => {
